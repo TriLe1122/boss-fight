5dd54b3b21a12f003712b10432d2d1681df34183
Kylee Want <Lumine3449@gmail.com>
2022-09-01 13:38:48 -0600

    boss-fight

---
+++ main.js
@@ -0,0 +1,38 @@
+const heros = [
+  {
+    name: 'Halbren Ironfist',
+    race: 'Dwarf',
+    damage: 10,
+    health: 100,
+  },
+  {
+    name: 'Dave Hammerhand',
+    race: 'Human',
+    damage: 5,
+    health: 50,
+  },
+  {
+    name: 'Halbren Ironfist',
+    race: 'Elf',
+    damage: 15,
+    health: 30,
+  },
+]
+
+const boss = {
+  health: 110,
+  maxHealth: 110,
+  damage: 5,
+  level: 1
+}
+
+
+//NOTE: GOALS
+/*Will demonstrate the ability to manipulate objects in an array and maintain an updated page with all the current data values. As well as working with an interval.
+
+*Add images
+
+*Label creatures
+
+*Add click attribute to boss for attacking it. This should reduce it with the COMBINED damaged from heros. Use look or forEach function to add up the damage than reduce boss
+*/
