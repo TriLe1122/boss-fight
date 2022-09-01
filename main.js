c6b39444af1b204d4ffb043373b0f1b36bc5d455
Kylee Want <Lumine3449@gmail.com>
2022-09-01 14:28:12 -0600

    heroes shop and monster

--- main.js
+++ main.js
@@ -27,7 +27,18 @@ const boss = {
 }
 
 
-//NOTE: GOALS
+
+
+function () {
+
+}
+
+
+
+
+
+
+//SECTION: GOALS
 /*Will demonstrate the ability to manipulate objects in an array and maintain an updated page with all the current data values. As well as working with an interval.
 
 *Add images
@@ -35,4 +46,14 @@ const boss = {
 *Label creatures
 
 *Add click attribute to boss for attacking it. This should reduce it with the COMBINED damaged from heros. Use look or forEach function to add up the damage than reduce boss
+
+*Boss damage per second (5?). Function that loops over heros array and reduces their health by boss damage.
+- give boss variable damage?
+
+Level up boss. Add logic to function that attacks the boss to determine if the boss is dead than increment level (increase max health and reset health to max) then re-draw boss after level up.
+- add alerts or indications that show how many monsters have been defeated/what level boss is on.
+
+*Give gold for killing boss that increases each level. Needs to be a global variable to hold the gold the hero has won, which can be displayed to the dom then.
+
+*Add 'health packs'
 */
